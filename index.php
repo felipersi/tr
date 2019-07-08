9ba18b896230e986ba5d234cc5aed49ca3cd3121
Luis <luis.felipersi@gmail.com>
2019-07-07 22:22:22 -0300

    a

--- index.php
+++ index.php
@@ -1,41 +1,50 @@
 <?php
-if(isset($argv)) {
+if(!empty($argv)) {
     $cidades = array('A', 'B', 'C', 'D');
 
-//$validaLetras = count($cidades);
-//if($validaLetras > 4 or $validaLetras < 1){
-//echo "\r\n";
-//echo "cidades incorretas";
-//echo "\r\n";
-//}
-    foreach ($argv as $arg) {
 
-        $primeraRota = substr($arg, 0);
-        $segundaRota = substr($arg, 1);
-        $terceiraRota = substr($arg, 2);
-
-        if ($primeraRota == $cidades[0]) {
-            echo "cidades " . $cidades[0];
+    $cidadesConsole = str_split(strtoupper($argv[1]));
+    $tamanhoRota = array_count_values($cidadesConsole);
+    $validaLocais = count($cidadesConsole);
+        if($validaLocais > 4){
+            echo "Não existe este destino";
             echo "\r\n";
-            break;
+            exit;
         }
-        elseif ($segundaRota !=  ) {
-            echo "cidades ". $cidades[1];
-            echo "\r\n";
-            break;
+        foreach($tamanhoRota as $valrota => $value) {
+            if ($value > 1) {
+                echo "A rota digitada contém cidades repetidas";
+                echo "\r\n";
+                exit;
+            }
         }
-        elseif ($terceiraRota = $cidades[2]) {
-            echo "cidades ". $cidades[2];
+        if ($cidadesConsole[0] == $cidades[0] && $cidadesConsole[1] == $cidades[1]) {
+            echo "cidades " . $cidades[0] . " para " . $cidades[1];
             echo "\r\n";
-            break;
+        }
+        elseif($cidadesConsole[0] == $cidades[0]) {
+            echo "cidades " . $city1;
+            echo "\r\n";
+        }
+        elseif ($segundaRota == $city2 ) {
+            echo "cidades " . $city2;
+            echo "\r\n";
+        }
+        elseif ($terceiraRota == $city3) {
+            echo "cidades " . $city3;
+            echo "\r\n";
         }
         else {
             echo "Nenhuma das condiões";
             echo "\r\n";
-            break;
         }
     }
-}
 
-echo $cidades[2];
+//echo $cidades[2];
 echo "\r\n";
+//echo $primeraRota;
+echo "\r\n";
+var_dump($validaLocais);
+echo "\r\n";
+
+
